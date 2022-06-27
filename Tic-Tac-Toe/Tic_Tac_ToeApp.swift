2b254992867a126ca2962e40ccebe942889f84e0
Mufeed AlMatar <almatarm@gmail.com>
2022-06-27 14:50:09 +0300

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Mufeed AlMatar on 28/11/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
