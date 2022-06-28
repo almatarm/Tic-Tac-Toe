dd15203394db4c315cd49559180875cb4403e48b
Mufeed AlMatar <almatarm@gmail.com>
2022-06-28 09:32:58 +0300

    Tap a grid item for  X and O indicator

--- Tic-Tac-Toe/ContentView.swift
+++ Tic-Tac-Toe/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var board: TicTacToe = TicTacToe(initPlayer: .human)
     
     let columns: [GridItem] = [ GridItem(.flexible()),
                                 GridItem(.flexible()),
@@ -28,10 +29,13 @@ struct ContentView: View {
                                 .frame(width: geometry.size.width/3 - 25,
                                        height: geometry.size.width/3 - 25)
                             
-                            Image(systemName: "xmark")
+                            Image(systemName: board.indicator(forIndex: i))
                                 .resizable()
                                 .frame(width: 35, height: 35)
                                 .foregroundColor(.white)
+                        }
+                        .onTapGesture {
+                            board.setSquare(forIndex: i)
                         }
                     }
                 })
@@ -41,10 +45,51 @@ struct ContentView: View {
             .padding()
         }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
+    }
+}
+
+struct TicTacToe {
+    private var moves: [Move?] = Array(repeatElement(nil, count: 9))
+    private var player: Player
+    
+    init(initPlayer: Player) {
+        self.player = initPlayer
+    }
+    
+    func isSquareOccupied(forIndex index: Int) -> Bool {
+        return moves[index] != nil
+    }
+    
+    mutating func setSquare(forIndex index: Int) -> Bool {
+        if isSquareOccupied(forIndex: index) {
+            return false
+        }
+        moves[index] = Move(player: player, boardIndex: index)
+        player = player == .human ? .computer : .human
+        return true
+    }
+    
+    func indicator(forIndex index: Int) -> String {
+        return moves[index]?.indicator ?? ""
+    }
+    
 }
